b4cb1f74931351ab21f1a9ee77babd78ee56d952
ZhenxingXiao <xiaozhenxing.albert@outlook.com>
2023-01-11 02:48:01 +0800

    - add footer

--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,7 @@
 use crate::app::App;
 use tui::{
     backend::Backend,
-    layout::{Constraint, Direction, Layout, Rect},
+    layout::{Constraint, Direction, Layout, Rect, Alignment},
     style::{Color, Modifier, Style},
     symbols,
     text::{Span, Spans},
@@ -15,9 +15,13 @@ use tui::{
 
 pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let chunks = Layout::default()
-        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Min(0),
+            Constraint::Length(3)].as_ref())
         .split(f.size());
-    draw_header(f, app, chunks[0])
+    draw_header(f, app, chunks[0]);
+    draw_footer(f, chunks[2]);
 }
 
 pub fn draw_header<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
@@ -26,6 +30,16 @@ pub fn draw_header<B: Backend>(f: &mut Frame<B>, app: &mut App, area: Rect) {
     let text = vec![
         Spans::from(app.title)
     ];
+    let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true }).alignment(Alignment::Center);
+    f.render_widget(paragraph, area);
+}
+
+pub fn draw_footer<B: Backend>(f: &mut Frame<B>, area: Rect){
+    let text: Vec<Spans> = vec![
+        Spans::from("💻 Press 'Q' to quit !")
+    ];
+    let block: Block = Block::default()
+        .borders(Borders::TOP | Borders::BOTTOM).border_type(BorderType::Plain);
     let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true });
     f.render_widget(paragraph, area);
 }
